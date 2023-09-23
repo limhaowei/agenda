7caafffcc30b233580b4ac63a24dc2759d99da29
Hao Wei Lim <94880011+limhaowei@users.noreply.github.com>
2023-09-23 23:04:25 +0800

    added days_left in models.py

--- agendapp/models.py
+++ agendapp/models.py
@@ -4,9 +4,17 @@ from django.utils import timezone
 # Create your models here.
 
 class Task(models.Model):
-    task = models.CharField(max_length=200)
-    desc = models.TextField()
+    task = models.TextField()
     deadline = models.DateTimeField(default=timezone.now)
+
+    def days_left(self):
+        now = timezone.now()
+        if self.deadline >= now:
+            delta = self.deadline - now
+            return delta.days
+        else:
+            return 0 
+
 
     def __str__(self):
         return self.task 
